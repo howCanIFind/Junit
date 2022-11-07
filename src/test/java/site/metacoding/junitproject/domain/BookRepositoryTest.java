2c68581ec05911f05aad6a194d11053ab16a0fd5
howCanIFind <second9000@naver.com>
2022-11-07 17:20:42 +0900

    Repository 테스트 환경 세팅 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,4 +1,26 @@
 package site.metacoding.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
+
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void 책등록_test() {
+        System.out.println("책등록_test 실행");
+    }
+
+    // 2. 책 목록 보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제제
 }
