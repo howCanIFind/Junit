494e3b0eba86cf2ff3c14651f93e038266ed4e7a
howCanIFind <second9000@naver.com>
2022-11-10 18:30:14 +0900

    select 테스트

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,11 +1,14 @@
 package site.metacoding.junitproject.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +17,20 @@ public class BookRepositoryTest {
 
     @Autowired // DI
     private BookRepository bookRepository;
+
+//    @BeforeAll // 테스트 시작 전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비() {
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
+    // 가정 1 : [ 데이터준비() + 1 책등록 ] (T), [ 데이터준비() + 2 책목록보기 ] (T) -> 사이즈 1 (검증 완료)
+    // 가정 2 : [ 데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기 ] (T) -> 사이즈 2 (검증 실패)
 
     // 1. 책 등록
     @Test
@@ -32,19 +49,37 @@ public class BookRepositoryTest {
         // then (검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
-    }
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 2. 책 목록 보기
     @Test
     public void 책목록보기_test() {
         // given
+        String title = "junit";
+        String author = "겟인데어";
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println(" 사이즈 : ====================================================== :" + booksPS.size());
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
+
+    // 3. 책 한건 보기
+    @Test
+    public void 책한건보기_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
 
         // when
-        List<Book> books = bookRepository.findAll();
+        Book booksPS = bookRepository.findById(1L).get();
+
         // then
-        System.out.println(books.size());
-    }
-    // 3. 책 한건 보기
+        assertEquals(title, booksPS.getTitle());
+        assertEquals(author, booksPS.getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 4. 책 수정
 
