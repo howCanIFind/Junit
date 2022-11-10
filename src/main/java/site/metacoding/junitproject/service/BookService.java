40f56007732c504a74f4860d1e2d7d7d02066f87
howCanIFind <second9000@naver.com>
2022-11-11 03:56:31 +0900

    북서비스 책등록 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,7 +1,31 @@
 package site.metacoding.junitproject.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책등록
+    @Transactional
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+
+    // 2. 책목록보기
+
+    // 3. 책한건보기
+
+    // 4. 책삭제
+
+    // 5. 책수정
 }
