9fc2ad499588b056ec63b238d0a5d802d4c5d3f7
howCanIFind <second9000@naver.com>
2022-11-11 07:29:17 +0900

    서비스 레이어테스트 - 실제 DB로 테스트하기

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,4 +1,39 @@
 package site.metacoding.junitproject.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 public class BookServiceTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록하기_테스트() {
+        // given
+        BookSaveReqDto dto = new BookSaveReqDto();
+        dto.setTitle("junit강의");
+        dto.setAuthor("메타코딩");
+
+        // stub
+        MailSenderStub mailSenderStub = new MailSenderStub();
+        // 가짜로 bookRepository 만들기
+
+        // when
+        BookService bookService = new BookService(bookRepository, mailSenderStub);
+        BookRespDto bookRespDto = bookService.책등록하기(dto);
+
+        //then
+        assertEquals(dto.getTitle(), bookRespDto.getTitle());
+        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+    }
 }
