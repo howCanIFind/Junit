3aa791861e847347177b4176ccdab35788d2f1b5
howCanIFind <second9000@naver.com>
2022-11-12 14:24:29 +0900

    서비스레이어 테스트 - 책삭제, 수정하기 테스트 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -58,11 +58,12 @@ public class BookService {
 
     // 5. 책수정
     @Transactional(rollbackFor = RuntimeException.class)
-    public void 책삭제하기(Long id, BookSaveReqDto dto) {
+    public BookRespDto 책수정하기(Long id, BookSaveReqDto dto) {
         Optional<Book> bookOP = bookRepository.findById(id);
         if (bookOP.isPresent()) {
             Book bookPS = bookOP.get();
-            bookPS.update(dto.getTitle(), dto.getAuthor());
+            bookPS.update(dto.getTitle(), dto.getAuthor()); // update 메서드 테스트를 못해본다.
+            return bookPS.toDto();
         } else {
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
         }
