d192868f2d5879c03d0399da69d3a87395d3402a
howCanIFind <second9000@naver.com>
2022-11-12 11:43:16 +0900

    서비스 레이어 테스트 - 가짜 환경 Mock 테스트 완료

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,21 +1,31 @@
 package site.metacoding.junitproject.service;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import site.metacoding.junitproject.domain.BookRepository;
-import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.util.MailSender;
 import site.metacoding.junitproject.web.dto.BookRespDto;
 import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    @Autowired
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private MailSender mailSender;
 
     @Test
     public void 책등록하기_테스트() {
@@ -24,16 +34,18 @@ public class BookServiceTest {
         dto.setTitle("junit강의");
         dto.setAuthor("메타코딩");
 
-        // stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
-        // 가짜로 bookRepository 만들기
+        // stub (가설)
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
 
         // when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookRespDto bookRespDto = bookService.책등록하기(dto);
 
         //then
-        assertEquals(dto.getTitle(), bookRespDto.getTitle());
-        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+//        assertEquals(dto.getTitle(), bookRespDto.getTitle());
+//        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
     }
 }
