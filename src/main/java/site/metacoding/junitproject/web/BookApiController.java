94c9810582a0bb2a89b454eeb81f394b9c15fa2a
howCanIFind <second9000@naver.com>
2022-11-14 10:46:37 +0900

    컨트롤러 레리어 본코드 - 책저장하기 유효성검사

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -3,6 +3,8 @@ package site.metacoding.junitproject.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,10 @@ import site.metacoding.junitproject.service.BookService;
 import site.metacoding.junitproject.web.dto.response.BookRespDto;
 import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
 import site.metacoding.junitproject.web.dto.response.CMRespDto;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,10 +27,23 @@ public class BookApiController { // 컴포지션 = has 관계
     // key=value&key=value
     // { "key": value, "key": value}
     @PostMapping("/api/v1/book")
-    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto bookSaveReqDto) {
+    public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveReqDto bookSaveReqDto, BindingResult bindingResult) {
         BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
-        CMRespDto<?> cmRespDto = CMRespDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build();
-        return new ResponseEntity<>(cmRespDto, HttpStatus.CREATED); // 201 = insert
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+            System.out.println("=====================================");
+            System.out.println(errorMap.toString());
+            System.out.println("=====================================");
+
+            return new ResponseEntity<>(CMRespDto.builder().code(-1).msg(errorMap.toString()).body(bookRespDto).build(),
+                    HttpStatus.BAD_REQUEST); // 400 = 요청이 잘못됨됨
+        }
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build(),
+                HttpStatus.CREATED); // 201 = insert
     }
 
     // 2. 책목록보기
--- src/main/java/site/metacoding/junitproject/web/dto/request/BookSaveReqDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/request/BookSaveReqDto.java
@@ -4,10 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import site.metacoding.junitproject.domain.Book;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter // Controller 에서 Setter 가 호출되면서 Dto 값이 채워짐
 public class BookSaveReqDto {
+
+    @Size(min = 1, max = 50)
+    @NotBlank
     private String title;
+
+    @Size(min = 2, max = 20)
+    @NotBlank
     private String author;
 
     public Book toEntity() {
